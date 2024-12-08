77a41c4cca923e65e40f588e40ed2f4bc9a8470d
Zaminali <zaminali.rustamov@azintelecom.az>
2024-12-08 13:27:47 +0400

    Added Dimens object

---
+++ app/src/main/java/az/lahza/hoqqaqr/ui/theme/Dimens.kt
@@ -0,0 +1,20 @@
+package az.lahza.hoqqaqr.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+object Dimens {
+    val ExtraSmall = 1.dp
+    val Small = 2.dp
+    val SmallMedium = 4.dp
+    val Medium = 6.dp
+    val MediumLarge = 8.dp
+    val Large = 10.dp
+    val ExtraLarge = 12.dp
+    val _2XLarge = 14.dp
+    val _3XLarge = 16.dp
+    val _4XLarge = 18.dp
+    val _5XLarge = 20.dp
+    val _6XLarge = 22.dp
+    val _7XLarge = 24.dp
+}
