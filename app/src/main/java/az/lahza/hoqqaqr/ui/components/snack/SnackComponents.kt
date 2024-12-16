9d258d16d516c308418b299f899fc0ef66a3df65
Zaminali <zaminali.rustamov@azintelecom.az>
2024-12-16 17:39:22 +0400

    Deleted unused import

--- app/src/main/java/az/lahza/hoqqaqr/ui/components/snack/SnackComponents.kt
+++ app/src/main/java/az/lahza/hoqqaqr/ui/components/snack/SnackComponents.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
